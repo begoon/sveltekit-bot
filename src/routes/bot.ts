7b807be9c214db2e6703950b8deb13923b6f1309
Alexander Demin <ademin@gmail.com>
2023-07-08 01:12:03 +0100

    Add async

--- src/routes/bot.ts
+++ src/routes/bot.ts
@@ -13,14 +13,14 @@ console.log("bot", BOT_TOKEN);
 
 export const history: any[] = [];
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
     history.push({
         when: new Date(),
         msg,
     });
-    bot.sendMessage(msg.chat.id, msg.text + ", and?");
+    await bot.sendMessage(msg.chat.id, msg.text + ", and?");
     if (msg.text === "glider") {
-        bot.sendPhoto(
+        await bot.sendPhoto(
             msg.chat.id,
             "https://upload.wikimedia.org/wikipedia/commons/4/45/Glider.svg"
         );
