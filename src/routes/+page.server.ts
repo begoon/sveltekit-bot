98994a905194232e1ac2f63bcc859a3b59417ccb
Alexander Demin <ademin@gmail.com>
2023-07-08 15:20:09 +0100

    Delete health endpoint

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,5 @@ import { bot, history } from "./bot";
 export async function load({ depends }) {
     const me = await bot.getWebHookInfo();
     depends("bot:refresh");
-    return {
-        webhook: me,
-        history,
-    };
+    return { webhook: me, history };
 }
