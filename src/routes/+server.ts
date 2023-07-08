907bb4ef2579496a5dfc82e81dc1ea6eb9a39c41
Alexander Demin <ademin@gmail.com>
2023-07-08 07:16:12 +0100

    Add try/catch

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,18 +1,13 @@
-import { env } from "$env/dynamic/private";
-import { json } from "@sveltejs/kit";
 import { bot } from "./bot";
 
-export async function GET() {
-    const me = env.BOT_URL;
-    console.log("me", me);
-    const wh = await bot.setWebHook(me!);
-    console.log(wh);
-    return json({ me, wh });
-}
-
 export async function POST({ request }) {
-    const data = await request.json();
-    console.log(data);
-    bot.processUpdate(data);
-    return new Response("", { status: 200 });
+    try {
+        const data = await request.json();
+        console.log(data);
+        bot.processUpdate(data);
+        return new Response("", { status: 200 });
+    } catch (e) {
+        console.error(e);
+        return new Response("", { status: 500 });
+    }
 }
--- src/routes/bot.ts
+++ src/routes/bot.ts
@@ -14,15 +14,19 @@ console.log("bot", BOT_TOKEN);
 export const history: any[] = [];
 
 bot.on("message", async (msg) => {
-    history.push({
-        when: new Date(),
-        msg,
-    });
-    await bot.sendMessage(msg.chat.id, msg.text + ", and?");
-    if (msg.text === "glider") {
-        await bot.sendPhoto(
-            msg.chat.id,
-            "https://upload.wikimedia.org/wikipedia/commons/4/45/Glider.svg"
-        );
+    try {
+        await bot.sendMessage(msg.chat.id, msg.text + ", and?");
+        if (msg.text === "glider") {
+            await bot.sendPhoto(
+                msg.chat.id,
+                "https://upload.wikimedia.org/wikipedia/commons/4/45/Glider.svg"
+            );
+        }
+        history.push({
+            when: new Date(),
+            msg,
+        });
+    } catch (e) {
+        console.error(e);
     }
 });
